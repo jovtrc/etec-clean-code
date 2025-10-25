ec4e050abed682ed5ed287fe8ab342e5b48c4746
João Carvalho <jovtrc@gmail.com>
2025-10-24 21:20:19 -0300

    adiciona entidades

---
+++ entities/product.entity.js
@@ -0,0 +1,16 @@
+class Product {
+  constructor({ id, name, price, quantity, createdAt }) {
+    if (!name || !price || !quantity) {
+      throw new Error("Nome, preço e a quantidade são obrigatórios");
+    }
+
+    this.id = id ? Number(id) : null;
+    this.name = String(name);
+    this.price = Number(price);
+    this.quantity = Number(quantity);
+
+    this.createdAt = createdAt ? new Date(createdAt) : new Date();
+  }
+}
+
+module.exports = Product;
